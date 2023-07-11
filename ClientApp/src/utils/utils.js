d0959c2c68d73d1dd6d90112df17946a32261892
Yuvateja Reddy <yuvatejareddy003@gmail.com>
2023-07-11 20:59:08 +0530

    [UPDATE] custom functions

--- ClientApp/src/utils/utils.js
+++ ClientApp/src/utils/utils.js
@@ -1,10 +1,14 @@
 import getStripe from "./get-stripe";
+import toast from "react-hot-toast"
 
 export const descriptionShow = (desc) => {
+  if(desc==null){
+    return ''
+  }
     if(desc.length<20){
         return desc
     }
-    return `${desc.substring(0,60)}...`
+    return `${desc.substring(0,50)}...`
 }
 
 export const postRequestOptions = {
@@ -21,11 +25,22 @@ export const getRequestOptions = {
                 }
             }
 
-export const deleteRequestOptins = {
-  
+export const deleteRequestOptions = {
+  method: "DELETE",
+  headers:{
+    'Authorization':`Bearer ${localStorage.getItem("authToken")}`
+  }
 }
 
-export const redirectToCheckout = async (prods) => {
+export const updateRequestOptions = {
+  method: "PUT",
+                headers: {
+                'Authorization':`Bearer ${localStorage.getItem("authToken")}`,
+                'Content-Type':'application/json'
+                }
+}
+
+export const redirectToCheckout = async (prods, deliveryAddressId) => {
         console.log(prods);
         let items = prods.map(prod =>{
                 return {id:prod.id, 
@@ -36,7 +51,7 @@ export const redirectToCheckout = async (prods) => {
             } )
           const requestData = {
             ...postRequestOptions,
-            body: JSON.stringify({items:items})
+            body: JSON.stringify({items:items, deliveryAddressId:deliveryAddressId})
           }
         const response = await fetch('/api/Payments/checkout',requestData)
         const stripe = await getStripe();
@@ -44,3 +59,18 @@ export const redirectToCheckout = async (prods) => {
         console.log(data);
         await stripe.redirectToCheckout({sessionId: data.id});
     }
+
+export function formatPrice(price){
+  return new Intl.NumberFormat('en-IN').format(price)
+}
+
+export function successToast(msg){
+  toast.success(msg,{
+    position: "top-right"
+  })
+}
+export function errorToast(msg){
+  toast.error(msg,{
+    position: "top-right"
+  })
+}
