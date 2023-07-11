c67b746e2206bb8603d298e34f0c758303a3622b
Yuvateja Reddy <yuvatejareddy003@gmail.com>
2023-07-11 20:57:42 +0530

    [ADD] Email Service

---
+++ Services/EmailService.cs
@@ -0,0 +1,29 @@
+﻿using System.Net.Mail;
+using System.Net;
+
+namespace ArtFusion.Services
+{
+    public class EmailService : IEmailService
+    {
+        public Task SendEmailAsync(string email, string subject, string message, List<string> ccList)
+        {
+            var client = new SmtpClient("smtp.gmail.com", 587)
+            {
+                EnableSsl = true,
+                UseDefaultCredentials = false,
+                Credentials = new NetworkCredential("mtejareddy1435@gmail.com", "wgnrfrhpvhyndyaw")
+            };
+            MailMessage mailMessage = new MailMessage(from: "mtejareddy1435@gmail.com",
+                                to: email,
+                                subject,
+                                message
+                                );
+            mailMessage.IsBodyHtml = true;
+            foreach(string cc in ccList )
+            {
+                mailMessage.Bcc.Add(cc);
+            }
+            return client.SendMailAsync(mailMessage);
+        }
+    }
+}
---
+++ Services/IEmailService.cs
@@ -0,0 +1,7 @@
+﻿namespace ArtFusion.Services
+{
+    public interface IEmailService
+    {
+        Task SendEmailAsync(string email, string subject, string message, List<string> ccList);
+    }
+}
